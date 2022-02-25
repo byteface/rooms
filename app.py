d71bafa4c55828019be4a602494e9d15b5d76164
byteface <byteface@googlemail.com>
2022-02-25 21:40:01 +0000

    aframe extras lib

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ from domonic.CDN import *
 _scripts = script("""
 AFRAME.registerComponent("foo", {
     init: function() {
-      this.portals = document.querySelectorAll('a[href]') // TODO - use a class?
+      this.portals = document.querySelectorAll('.portal') // TODO - use a class?
+      console.log("PPP", this.portals)
       //this.box = document.querySelector('a-box')
       //this.text = document.querySelector("a-text")
     },
@@ -46,6 +47,12 @@ AFRAME.registerComponent("foo", {
 _webpage = lambda scene: html(head(
     script(
         _src=CDN_JS.AFRAME_1_2),
+
+        # this bugs the controls?
+        # <script src="https://unpkg.com/aframe-fps-look-controls-component/dist/aframe-fps-look-controls-component.min.js"></script>
+
+        script(_src="//cdn.rawgit.com/donmccurdy/aframe-extras/v4.1.1/dist/aframe-extras.min.js"),
+
         _scripts
     ),
     body(
@@ -56,28 +63,28 @@ _webpage = lambda scene: html(head(
 
 # lobby
 _scene = scene(
-      box(_position="-10 0.5 -3", _rotation="0 45 0", _color="#4CC3D9", _href="/home"),
-      sphere(_position="0 1.25 -5", _radius="1.25", _color="#EF2D5E", _href="/room"),
+      box(_position="-10 0.5 -3", _rotation="0 45 0", _color="#4CC3D9", _class="portal", _href="/home"),
+      sphere(_position="0 1.25 -5", _radius="1.25", _color="#EF2D5E", _class="portal", _href="/room"),
       cylinder(_position="1 0.75 -3", _radius="0.5", _height="1.5", _color="#FFC65D"),
       plane(_position="0 0 -4", _rotation="-90 0 0", _width="40", _height="4", _color="#7BC8A4"),
+      # entity(_text="value: Welcome to the rooms. Go an explore!", material="color: blue", _geometry="primitive: plane; width: 4; height: auto"),
       sky(_color="#ECECEC"),
-      '<a-camera foo><a-camera>', # TOOD - domonic can't do 'no attribute'
+      '<a-camera foo><a-camera>',
     )
 
 # default home without a user edit
 _home = scene(
-      box(_position="-1 0.5 -5", _rotation="45 45 0", _color="#4CC3D9", _href="/"),
+      box(_position="-1 0.5 -5", _rotation="45 45 0", _color="#4CC3D9", _class="portal", _href="/"),
       sphere(_position="2 1.25 -5", _radius="1.25", _color="#EF2D5E"),
       plane(_position="0 0 -4", _rotation="-90 0 0", _width="40", _height="4", _color="#7BC8A4"),
       sky(_color="#ECECEC"),
       '<a-camera foo><a-camera>',
     )
-
-
+            
 app = FastAPI()
 
 # lobby
-@lru_cache
+# @lru_cache
 @app.get("/")
 def root():
     return HTMLResponse(str(
@@ -85,7 +92,7 @@ def root():
     ))
 
 # users own room. editable
-@lru_cache
+# @lru_cache
 @app.get("/home")
 def home(username=None):
     return HTMLResponse(str(
@@ -95,13 +102,14 @@ def home(username=None):
 @app.get("/room")
 def room():
     room = scene(
-      box(_position="-1 0.5 -3", _rotation="0 45 0", _color="#4CC3D9"),
-      plane(_position="0 0 -4", _rotation="-90 0 0", _width="40", _height="4", _color="#7BC8A4"),
+      box(_position="-1 0.5 -3", _rotation="0 45 0", _color="#4CC3D9", _href="/home"),
+      plane(_position="0 0 -4", _rotation="-90 0 0", _width="400", _height="400", _color="#7BC8A4"),
       sky(_color="#ECECEC"),
-      '<a-camera foo><a-camera>',
+      entity( _id="rig", **{"_movement-controls":"fly: true; speed: 0.3"}, _position="25 0 25"),
+      '<a-camera camera look-controls="pointerLockEnabled: true"><a-camera>',
     )
     return HTMLResponse(str(
-        room
+        _webpage(room)
     ))
 
 # generate a random room
@@ -111,10 +119,10 @@ def random_room():
       box(_position="-1 0.5 -3", _rotation="0 45 0", _color="#4CC3D9"),
       # plane(_position="0 0 -4", _rotation="-90 0 0", _width="40", _height="4", _color="#7BC8A4"),
       sky(_color="#ECECEC"),
-      '<a-camera foo><a-camera>',
+      '<a-camera camera><a-camera>',
     )
     return HTMLResponse(str(
-        room
+        _webpage(room)
     ))
 
 
